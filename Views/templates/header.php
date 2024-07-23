af9daa18f7befaa9a281ba78e903530cee3f68ee
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-07-23 13:53:08 -0500

    agregar_index

--- Views/templates/header.php
+++ Views/templates/header.php
@@ -22,6 +22,7 @@ if (json_last_error() !== JSON_ERROR_NONE) {
 }
 
 // Define las constantes
+define('FAVICON', $data[0]['favicon']);
 define('LOGO_TIENDA', $data[0]['logo_url']);
 define('COLOR_BACKGROUND', $data[0]['color']);
 define('COLOR_BOTONES', $data[0]['color_botones']);
@@ -70,7 +71,7 @@ $primera_seccion = obtenerPrimeraSeccion($url);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tienda</title>
-    <link rel="icon" href="https://marketing4ecommerce.net/wp-content/uploads/2024/02/imagen-generada-con-nightcafe-e1708680739301.jpg" type="image/x-icon">
+    <link rel="icon" href="<?php echo SERVERURL . FAVICON; ?>" type="image/x-icon">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
