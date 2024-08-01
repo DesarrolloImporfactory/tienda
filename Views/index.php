c9b2c3308f18fd3cee6102fe91a01a27f1d72005
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-08-01 11:34:22 -0500

    agregar_index

--- Views/index.php
+++ Views/index.php
@@ -359,7 +359,7 @@
                                     <div class="card card_icon text-center">
                                         <div class="card-body card-body_icon d-flex flex-row">
                                             <div style="margin-right: 20px;">
-                                                <i class="fas ${icon_text} fa-2x"></i> <!-- Cambia el icono según corresponda -->
+                                                <i class="fa ${icon_text} fa-2x"></i> <!-- Cambia el icono según corresponda -->
                                             </div>
                                             <div>
                                                 <h5 class="card-title card-title_icon">${texto}</h5>
