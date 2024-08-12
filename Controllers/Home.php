58548e818a15b4bb9d96f79e62f6412f402107f5
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-08-12 10:17:00 -0500

    agregar_index

--- Controllers/Home.php
+++ Controllers/Home.php
@@ -3,16 +3,29 @@
 use FontLib\EOT\Header;
 
 session_start();
+
 class Home extends Controller
 {
+    private $homeModel;
+
     public function __construct()
     {
         parent::__construct();
     }
+
     ///Vistas
     public function index()
     {
-        $this->views->render($this, "index");
+        $id_plataforma = ID_PLATAFORMA;
+        $home = $this->model->plantilla($id_plataforma);
+
+        // Condicional dependiendo del resultado
+        if ($home == 1) {
+            // Renderizar la vista normalmente si hay productos
+            $this->views->render($this, "index");
+        } else if ($home == 2) {
+            // Renderizar la vista normalmente si hay productos
+            $this->views->render($this, "index2");
+        }
     }
-    ///Funciones
 }
