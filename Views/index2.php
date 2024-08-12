71e39d8ff9d6357aab5bbb6ee4961b14f446308f
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-08-12 14:32:58 -0500

    agregar_index

--- Views/index2.php
+++ Views/index2.php
@@ -1,4 +1,183 @@
 <?php include 'Views/templates/header2.php'; ?>
 
 
+<?php
+include 'includes/header.php';
+?>
+<main style="background-color: #f9f9f9;">
+    <div id="carouselExampleFade" class="carousel slide carousel-fade custom-carousel" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="https://static.wixstatic.com/media/c837a6_9c1280daaeb0481abc58e6e236efdf59~mv2.png/v1/fill/w_980,h_565,al_br,q_90,usm_0.66_1.00_0.01,enc_auto/c837a6_9c1280daaeb0481abc58e6e236efdf59~mv2.png" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <div class="tag">Mejores precios</div>
+                    <h1>Súper precios en tus artículos favoritos</h1>
+                    <p>Gana más por tu dinero</p>
+                    <button class="btn btn-primary">Comprar ahora</button>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="https://static.wixstatic.com/media/c837a6_f58829a26e594ca3aa72383e19cf39b9~mv2.png/v1/fill/w_980,h_565,al_r,q_90,usm_0.66_1.00_0.01,enc_auto/c837a6_f58829a26e594ca3aa72383e19cf39b9~mv2.png" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <div class="tag">Grandes descuentos</div>
+                    <h1>No te pierdas nuestras ofertas exclusivas</h1>
+                    <p>Obtén más por menos</p>
+                    <button class="btn btn-primary">Comprar ahora</button>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="https://static.wixstatic.com/media/c837a6_9c1280daaeb0481abc58e6e236efdf59~mv2.png/v1/fill/w_980,h_565,al_br,q_90,usm_0.66_1.00_0.01,enc_auto/c837a6_9c1280daaeb0481abc58e6e236efdf59~mv2.png" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <div class="tag">Nuevas llegadas</div>
+                    <h1>Explora los últimos productos de tecnología</h1>
+                    <p>Descubre lo más reciente</p>
+                    <button class="btn btn-primary">Comprar ahora</button>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    <!-- Seccion de ofertas y promociones -->
+    <div class="seccion">
+        <div class="flex_seccionOfertas">
+            <div class="promotion-card">
+                <img src="assets/imagenes/telefono_rojo.webp" class="promotion-image" alt="Smartphone">
+                <div class="promotion-content">
+                    <h2>Ofertas de temporada</h2>
+                    <h1>Hasta 30% menos</h1>
+                    <p>En Smartphones seleccionados</p>
+                    <button class="btn btn-light">Tienda</button>
+                </div>
+            </div>
+            <div class="promotion-card">
+                <img src="assets/imagenes/audifonos_morados.webp" class="promotion-image" alt="Headphones">
+                <div class="promotion-content">
+                    <h2>Recién llegados</h2>
+                    <h1>Lleva tu sonido donde quieras</h1>
+                    <p>Mejores marcas de audífonos</p>
+                    <button class="btn btn-light">Comprar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="seccion">
+        <div class="caja">
+            <div class="seccion_iconos">
+                <div class="d-flex flex-row">
+                    <i class='bx bx-timer menu-icon'></i>
+                    <span>Disponible para ti 24/7 </span>
+                </div>
+                <div class="d-flex flex-row">
+                    <i class='bx bx-money-withdraw menu-icon'></i>
+                    <span>Precios bajos garantizados</span>
+                </div>
+                <div class="d-flex flex-row">
+                    <i class="fa-solid fa-headset menu-icon"></i>
+                    <span>Atención al cliente</span>
+                </div>
+                <div class="d-flex flex-row">
+                    <i class='bx bxs-lock menu-icon'></i>
+                    <span>Pago Contra Entrega </span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="mas_vendidos">
+        <div class="caja">
+            <h2>Más vendidos</h2>
+            <div class="flex_mas_vendidos">
+                <div class="mas_vendidos-card">
+                    <div class="mas_vendidos-tag">OFERTA</div>
+                    <div class="mas_vendidos-image-wrapper">
+                        <img src="https://marketing4ecommerce.net/wp-content/uploads/2024/02/imagen-generada-con-nightcafe-e1708680739301.jpg" class="mas_vendidos-image" alt="Product 1">
+                    </div>
+                    <div class="mas_vendidos-info">
+                        <p>Fitboot fitness con seguimiento del ritmo cardíaco</p>
+                        <p class="mas_vendidos-price"><span class="mas_vendidos-old-price">$999.00</span> $984.00</p>
+                    </div>
+                </div>
+                <div class="mas_vendidos-card">
+                    <div class="mas_vendidos-tag">OFERTA</div>
+                    <div class="mas_vendidos-image-wrapper">
+                        <img src="https://marketing4ecommerce.net/wp-content/uploads/2024/02/imagen-generada-con-nightcafe-e1708680739301.jpg" class="mas_vendidos-image" alt="Product 2">
+                    </div>
+                    <div class="mas_vendidos-info">
+                        <p>JP Laptop para juegos de 15,6" de 256GB</p>
+                        <p class="mas_vendidos-price"><span class="mas_vendidos-old-price">$999.00</span> $984.00</p>
+                    </div>
+                </div>
+                <div class="mas_vendidos-card">
+                    <div class="mas_vendidos-tag">OFERTA</div>
+                    <div class="mas_vendidos-image-wrapper">
+                        <img src="https://marketing4ecommerce.net/wp-content/uploads/2024/02/imagen-generada-con-nightcafe-e1708680739301.jpg" class="mas_vendidos-image" alt="Product 3">
+                    </div>
+                    <div class="mas_vendidos-info">
+                        <p>HKI Tech drone cuadricóptero con cámara y mando 360</p>
+                        <p class="mas_vendidos-price">$999.00</p>
+                    </div>
+                </div>
+                <div class="mas_vendidos-card">
+                    <div class="mas_vendidos-tag">OFERTA</div>
+                    <div class="mas_vendidos-image-wrapper">
+                        <img src="https://marketing4ecommerce.net/wp-content/uploads/2024/02/imagen-generada-con-nightcafe-e1708680739301.jpg" class="mas_vendidos-image" alt="Product 4">
+                    </div>
+                    <div class="mas_vendidos-info">
+                        <p>Smartphone Z Pixel Max 128GB desbloqueado</p>
+                        <p class="mas_vendidos-price"><span class="mas_vendidos-old-price">$999.00</span> $984.00</p>
+                    </div>
+                </div>
+                <div class="mas_vendidos-card">
+                    <div class="mas_vendidos-tag">OFERTA</div>
+                    <div class="mas_vendidos-image-wrapper">
+                        <img src="https://marketing4ecommerce.net/wp-content/uploads/2024/02/imagen-generada-con-nightcafe-e1708680739301.jpg" class="mas_vendidos-image" alt="Product 5">
+                    </div>
+                    <div class="mas_vendidos-info">
+                        <p>Audífonos inalámbricos con cancelación del ruido</p>
+                        <p class="mas_vendidos-price"><span class="mas_vendidos-old-price">$999.00</span> $984.00</p>
+                    </div>
+                </div>
+                <div class="mas_vendidos-card">
+                    <div class="mas_vendidos-tag">OFERTA</div>
+                    <div class="mas_vendidos-image-wrapper">
+                        <img src="https://marketing4ecommerce.net/wp-content/uploads/2024/02/imagen-generada-con-nightcafe-e1708680739301.jpg" class="mas_vendidos-image" alt="Product 6">
+                    </div>
+                    <div class="mas_vendidos-info">
+                        <p>Gafas de realidad virtual Safay GEN 2 de 256 GB con mandos táctiles</p>
+                        <p class="mas_vendidos-price">$999.00</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Sección Ahorra -->
+    <div class="ahorro-section">
+        <div class="ahorro-image">
+            <img src="https://static.wixstatic.com/media/c837a6_42dd66a436e846648736f4bc9546bf14~mv2.png/v1/fill/w_1825,h_600,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/c837a6_42dd66a436e846648736f4bc9546bf14~mv2.png" alt="Producto">
+            <!-- <div class="circle-badge">Mejor precio</div> -->
+        </div>
+        <div class="ahorro-content">
+            <div class="text-content  d-flex flex-column">
+                <h2>Ahorra hasta</h2>
+                <h1>$150</h1>
+                <p>en laptops y tabletas seleccionadas</p>
+                <small>Aplican los términos y condiciones</small>
+                <button class="tienda-btn">Tienda</button>
+            </div>
+        </div>
+    </div>
+    <!-- Fin Sección Ahorra -->
+
+</main>
+<?php
+include 'includes/footer.php';
+?>
+
+
 <?php include 'Views/templates/footer2.php'; ?>
