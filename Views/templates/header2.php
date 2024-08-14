e3f27f39d1e58aa87c26243c7fd0a71654c57f15
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-08-14 14:41:36 -0500

    agregar_index

--- Views/templates/header2.php
+++ Views/templates/header2.php
@@ -177,7 +177,7 @@ $primera_seccion = obtenerPrimeraSeccion();
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#"><?php echo NOMBRE_TIENDA; ?></a>
+                <a class="navbar-brand" href="<?php echo $primera_seccion; ?>"><?php echo NOMBRE_TIENDA; ?></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar" aria-controls="mainNavbar" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
