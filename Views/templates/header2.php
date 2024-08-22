d3adbb1ce259a643fa4adb1bedd79652e1b3a95f
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-08-22 10:18:32 -0500

    agregar_index

--- Views/templates/header2.php
+++ Views/templates/header2.php
@@ -223,7 +223,7 @@ $primera_seccion = obtenerPrimeraSeccion();
                 </div>
             </div>
         </nav>
-        <button class="floating-button" id="cartDropdown">
+        <button class="floating-button">
             <i class="fas fa-shopping-cart"></i>
         </button>
         <!-- Sub-Nav -->
