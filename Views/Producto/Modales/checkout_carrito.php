b6aec46083801fd939c450ffcbb1ec7a944b35e7
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-09-06 16:18:52 -0500

    agregar_index

--- Views/Producto/Modales/checkout_carrito.php
+++ Views/Producto/Modales/checkout_carrito.php
@@ -381,8 +381,9 @@
                         <div class="card p-3 mb-3" style="border: 1px solid #007bff; background-color: #e9f4ff; width: 90%; align-self: center;" id="seccion_oferta">
                             <div class="d-flex justify-content-start align-items-center">
                                 <input type="checkbox" id="envioPrioritario" class="me-2" onchange="toggleEnvioPrioritario(this)">
+                                <input type="hidden" id="id_producto_oferta" name="id_producto_oferta">
                                 <label for="envioPrioritario" class="m-0">
-                                    <strong><span style="font-weight:bold;" id="nombre_oferta">ENVÍO PRIORITARIO + GARANTÍA</span></strong> por solo
+                                    <strong><span style="font-weight:bold;" id="nombre_oferta"></span></strong> por solo
                                     <strong><span id="precio_oferta"></span></strong>
                                 </label>
                             </div>
