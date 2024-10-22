1c08ab3c1597ffa6c85f1d9f5f97bc8e9d2e7f24
Carlos Cisneros <cisnerosgranda14@gmail.com>
2024-10-22 12:29:19 -0500

    bug nav

--- Views/index3.php
+++ Views/index3.php
@@ -344,7 +344,7 @@
                         <hr>
                         <p class="card-text">Some quick example text to build on the card title and make up the
                             bulk
-                            of the card's content.</p>
+                            of the card's content</p>
                         <button type="button" class="btn btn-primary w-100" data-bs-toggle="modal"
                             data-bs-target="#modalDortores">
                             Mas sobre el doctor
