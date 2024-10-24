8303355a40e9e08fa9a495b24b0f899e8e000dbe
Carlos Cisneros <cisnerosgranda14@gmail.com>
2024-10-23 22:36:25 -0500

    pagina productos

--- Views/Producto3/index.php
+++ Views/Producto3/index.php
@@ -162,7 +162,7 @@
     </nav>
 
 
-    <header class="mt-5">
+    <header class="pt-5">
         <div class="container containerProductos3">
             <div class="d-flex justify-content-between align-items-center border-bottom border-2">
                 <h3 class="display-6 fw-bold texto-secondary">Productos </h3>
