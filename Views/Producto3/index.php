e9b8a589f04dd0fd0742464972ba086a4cd218d7
Carlos Cisneros <cisnerosgranda14@gmail.com>
2024-10-30 12:55:08 -0500

    offcanvas

--- Views/Producto3/index.php
+++ Views/Producto3/index.php
@@ -9,7 +9,7 @@
         <div class="cont2Productos row pt-4">
 
             <div class="filtro col-12 col-sm-6 col-md-3">
-                <div class="x sticky-md-top">
+                <div class="x sticky-sm-top">
 
                     <div class="form-floating mb-3">
                         <input type="text" class="form-control" id="buscador" placeholder="Buscar por nombre"
