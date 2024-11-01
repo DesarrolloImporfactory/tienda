0b481faabce5e49b05a053cf930e994d9c940a32
Carlos Cisneros <cisnerosgranda14@gmail.com>
2024-10-31 20:57:42 -0500

    estilos plantilla 1

--- Views/index.php
+++ Views/index.php
@@ -366,8 +366,8 @@
                                 <a ${enlaceHTML}>
                                     <div class="card card_icon text-center">
                                         <div class="card-body card-body_icon d-flex flex-row justify-content-between align-items-center" >
-                                            <i class="fa ${icon_text} fa-2x me-4" style="color: ${icono.color_icono} !important"></i>
-                                            <div>
+                                            <i class="fa ${icon_text} fa-2x me-3" style="color: ${icono.color_icono} !important"></i>
+                                            <div class="text-end">
                                                 <h5 class="card-title card-title_icon">${texto}</h5>
                                                 <p class="card-text card-text_icon" style="font-size: 12px;">${subtexto_icon}</p>
                                             </div>
