99688f316e56c1d89441cfad9c2a9d3adbcc3b86
Carlos Cisneros <cisnerosgranda14@gmail.com>
2024-11-08 15:18:04 -0500

    Arreglo pagina producto p1

--- Views/Producto/index.php
+++ Views/Producto/index.php
@@ -95,8 +95,8 @@ $id_producto = $_GET['id'];
                         <div id="ahorra-container" class="px-2"
                             style="background-color: #4464ec; color:white !important; border-radius: 0.3rem;">
                             <p class="ahorra mb-0 d-flex align-items-center">
-                                <i class="bx bxs-purchase-tag me-3" style="color: white;"></i>
-                                <span id="ahorra"></span> 
+                                <i class="bx bxs-purchase-tag" style="color: white;"></i>
+                                <span id="ahorra" class="ms-3"></span> 
                             </p>
                         </div>
                     </div>
