fb2fe213f7f25250c47e68d27ffc5a14c8cf5d13
Carlos Cisneros <cisnerosgranda14@gmail.com>
2024-11-08 20:27:24 -0500

    Arreglo pagina producto p1

--- Views/Producto3/index.php
+++ Views/Producto3/index.php
@@ -4,9 +4,18 @@
 
 <main>
     <section>
-        <div class="container">
-            <div class="row">
-                <div class="col">ddd</div>
+        <div class="container containerProducto3">
+            <div class="row rowProducto3">
+                <div class="col">
+                    <div class="d-flex flex-column">
+                        <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSWzLcnrQcRxHe0xxyr8bQQMd1oz6iqoLJlxWaCYUIMYi9zDjF2U2VO7Aldk5aUAs57iO-3H-gpby8Qql8_M3MDQVP4AMU1V4gmW-RDT0Ey1yHk23lUuT5XJDj1p0bLO5rXKrfXwA&usqp=CAc" alt="">
+                        <div class="d-flex gap-3" style="">
+                            <img width="40" height="40" src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSWzLcnrQcRxHe0xxyr8bQQMd1oz6iqoLJlxWaCYUIMYi9zDjF2U2VO7Aldk5aUAs57iO-3H-gpby8Qql8_M3MDQVP4AMU1V4gmW-RDT0Ey1yHk23lUuT5XJDj1p0bLO5rXKrfXwA&usqp=CAc" alt="">
+                            <img width="40" height="40" src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSWzLcnrQcRxHe0xxyr8bQQMd1oz6iqoLJlxWaCYUIMYi9zDjF2U2VO7Aldk5aUAs57iO-3H-gpby8Qql8_M3MDQVP4AMU1V4gmW-RDT0Ey1yHk23lUuT5XJDj1p0bLO5rXKrfXwA&usqp=CAc" alt="">
+                            <img width="40" height="40" src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSWzLcnrQcRxHe0xxyr8bQQMd1oz6iqoLJlxWaCYUIMYi9zDjF2U2VO7Aldk5aUAs57iO-3H-gpby8Qql8_M3MDQVP4AMU1V4gmW-RDT0Ey1yHk23lUuT5XJDj1p0bLO5rXKrfXwA&usqp=CAc" alt="">
+                        </div>
+                    </div>
+                </div>
                 <div class="col">sss</div>
             </div>
         </div>
