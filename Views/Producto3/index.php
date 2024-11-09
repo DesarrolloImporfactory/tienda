ccca66026f8bc9435dfbf55fdcd98b9a0a8924f8
Carlos Cisneros <cisnerosgranda14@gmail.com>
2024-11-08 20:15:26 -0500

    Arreglo pagina producto p1

--- Views/Producto3/index.php
+++ Views/Producto3/index.php
@@ -1,7 +1,14 @@
 <?php include 'Views/templates/header3.php'; ?>
 
 <main>
-    hola
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col"></div>
+                <div class="col"></div>
+            </div>
+        </div>
+    </section>
 </main>
 
 <?php include 'Views/templates/footer3.php'; ?>
