ef491a1324bfae23633542d731355b9d0344ece7
Carlos Cisneros <cisnerosgranda14@gmail.com>
2024-11-08 20:43:18 -0500

    Arreglo pagina producto p1

--- Views/Producto3/index.php
+++ Views/Producto3/index.php
@@ -8,7 +8,7 @@
             <div class="row rowProducto3">
                 <div class="col">
                     <div class="d-flex flex-column">
-                        <img style="width: 400px; height: 400px;" class="border rounded" src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSWzLcnrQcRxHe0xxyr8bQQMd1oz6iqoLJlxWaCYUIMYi9zDjF2U2VO7Aldk5aUAs57iO-3H-gpby8Qql8_M3MDQVP4AMU1V4gmW-RDT0Ey1yHk23lUuT5XJDj1p0bLO5rXKrfXwA&usqp=CAc" alt="">
+                        <img style="width: 400px; height: 400px;" class="border rounded mx-auto" src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSWzLcnrQcRxHe0xxyr8bQQMd1oz6iqoLJlxWaCYUIMYi9zDjF2U2VO7Aldk5aUAs57iO-3H-gpby8Qql8_M3MDQVP4AMU1V4gmW-RDT0Ey1yHk23lUuT5XJDj1p0bLO5rXKrfXwA&usqp=CAc" alt="">
                         <div style="max-width: 400px;" class="d-flex gap-3 mt-3 w-100" >
                             <img class="rounded border " width="60" height="60" src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSWzLcnrQcRxHe0xxyr8bQQMd1oz6iqoLJlxWaCYUIMYi9zDjF2U2VO7Aldk5aUAs57iO-3H-gpby8Qql8_M3MDQVP4AMU1V4gmW-RDT0Ey1yHk23lUuT5XJDj1p0bLO5rXKrfXwA&usqp=CAc" alt="">
                             <img class="rounded border " width="60" height="60" src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSWzLcnrQcRxHe0xxyr8bQQMd1oz6iqoLJlxWaCYUIMYi9zDjF2U2VO7Aldk5aUAs57iO-3H-gpby8Qql8_M3MDQVP4AMU1V4gmW-RDT0Ey1yHk23lUuT5XJDj1p0bLO5rXKrfXwA&usqp=CAc" alt="">
@@ -19,7 +19,7 @@
                 <div class="col">sss</div>
             </div>
         </div>
-    </section>
+    </section> 
 </main>
 
 <?php include 'Views/templates/footer3.php'; ?>
