ea2141372b4cc9ecb056876be4f8efb277b2b471
Carlos Cisneros <cisnerosgranda14@gmail.com>
2024-11-08 20:32:09 -0500

    Arreglo pagina producto p1

--- Views/Producto3/index.php
+++ Views/Producto3/index.php
@@ -8,11 +8,11 @@
             <div class="row rowProducto3">
                 <div class="col">
                     <div class="d-flex flex-column">
-                        <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSWzLcnrQcRxHe0xxyr8bQQMd1oz6iqoLJlxWaCYUIMYi9zDjF2U2VO7Aldk5aUAs57iO-3H-gpby8Qql8_M3MDQVP4AMU1V4gmW-RDT0Ey1yHk23lUuT5XJDj1p0bLO5rXKrfXwA&usqp=CAc" alt="">
-                        <div class="d-flex gap-3" style="">
-                            <img width="40" height="40" src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSWzLcnrQcRxHe0xxyr8bQQMd1oz6iqoLJlxWaCYUIMYi9zDjF2U2VO7Aldk5aUAs57iO-3H-gpby8Qql8_M3MDQVP4AMU1V4gmW-RDT0Ey1yHk23lUuT5XJDj1p0bLO5rXKrfXwA&usqp=CAc" alt="">
-                            <img width="40" height="40" src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSWzLcnrQcRxHe0xxyr8bQQMd1oz6iqoLJlxWaCYUIMYi9zDjF2U2VO7Aldk5aUAs57iO-3H-gpby8Qql8_M3MDQVP4AMU1V4gmW-RDT0Ey1yHk23lUuT5XJDj1p0bLO5rXKrfXwA&usqp=CAc" alt="">
-                            <img width="40" height="40" src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSWzLcnrQcRxHe0xxyr8bQQMd1oz6iqoLJlxWaCYUIMYi9zDjF2U2VO7Aldk5aUAs57iO-3H-gpby8Qql8_M3MDQVP4AMU1V4gmW-RDT0Ey1yHk23lUuT5XJDj1p0bLO5rXKrfXwA&usqp=CAc" alt="">
+                        <img style="width: 400px; height: 400px;" class="border rounded mx-auto" src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSWzLcnrQcRxHe0xxyr8bQQMd1oz6iqoLJlxWaCYUIMYi9zDjF2U2VO7Aldk5aUAs57iO-3H-gpby8Qql8_M3MDQVP4AMU1V4gmW-RDT0Ey1yHk23lUuT5XJDj1p0bLO5rXKrfXwA&usqp=CAc" alt="">
+                        <div class="d-flex gap-3 mt-3" style="">
+                            <img class="rounded border " width="60" height="60" src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSWzLcnrQcRxHe0xxyr8bQQMd1oz6iqoLJlxWaCYUIMYi9zDjF2U2VO7Aldk5aUAs57iO-3H-gpby8Qql8_M3MDQVP4AMU1V4gmW-RDT0Ey1yHk23lUuT5XJDj1p0bLO5rXKrfXwA&usqp=CAc" alt="">
+                            <img class="rounded border " width="60" height="60" src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSWzLcnrQcRxHe0xxyr8bQQMd1oz6iqoLJlxWaCYUIMYi9zDjF2U2VO7Aldk5aUAs57iO-3H-gpby8Qql8_M3MDQVP4AMU1V4gmW-RDT0Ey1yHk23lUuT5XJDj1p0bLO5rXKrfXwA&usqp=CAc" alt="">
+                            <img class="rounded border " width="60" height="60" src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSWzLcnrQcRxHe0xxyr8bQQMd1oz6iqoLJlxWaCYUIMYi9zDjF2U2VO7Aldk5aUAs57iO-3H-gpby8Qql8_M3MDQVP4AMU1V4gmW-RDT0Ey1yHk23lUuT5XJDj1p0bLO5rXKrfXwA&usqp=CAc" alt="">
                         </div>
                     </div>
                 </div>
