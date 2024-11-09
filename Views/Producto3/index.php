21e0b2f4ed512cea8def67bef722dd5fb080f627
Carlos Cisneros <cisnerosgranda14@gmail.com>
2024-11-08 21:02:33 -0500

    Arreglo pagina producto p1

--- Views/Producto3/index.php
+++ Views/Producto3/index.php
@@ -9,7 +9,7 @@
                 <div class="col">
                     <div class="d-flex flex-column">
                         <img style="width: 400px; height: 400px;" class="border rounded mx-auto" src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSWzLcnrQcRxHe0xxyr8bQQMd1oz6iqoLJlxWaCYUIMYi9zDjF2U2VO7Aldk5aUAs57iO-3H-gpby8Qql8_M3MDQVP4AMU1V4gmW-RDT0Ey1yHk23lUuT5XJDj1p0bLO5rXKrfXwA&usqp=CAc" alt="">
-                        <div style="max-width: 400px;" class="d-flex gap-3 mt-3 w-100 mx-auto" >
+                        <div id="list-tab" style="max-width: 400px;" class="d-flex gap-3 mt-3 w-100 mx-auto" >
                             <img class="rounded border " width="60" height="60" src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSWzLcnrQcRxHe0xxyr8bQQMd1oz6iqoLJlxWaCYUIMYi9zDjF2U2VO7Aldk5aUAs57iO-3H-gpby8Qql8_M3MDQVP4AMU1V4gmW-RDT0Ey1yHk23lUuT5XJDj1p0bLO5rXKrfXwA&usqp=CAc" alt="">
                             <img class="rounded border " width="60" height="60" src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSWzLcnrQcRxHe0xxyr8bQQMd1oz6iqoLJlxWaCYUIMYi9zDjF2U2VO7Aldk5aUAs57iO-3H-gpby8Qql8_M3MDQVP4AMU1V4gmW-RDT0Ey1yHk23lUuT5XJDj1p0bLO5rXKrfXwA&usqp=CAc" alt="">
                             <img class="rounded border " width="60" height="60" src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSWzLcnrQcRxHe0xxyr8bQQMd1oz6iqoLJlxWaCYUIMYi9zDjF2U2VO7Aldk5aUAs57iO-3H-gpby8Qql8_M3MDQVP4AMU1V4gmW-RDT0Ey1yHk23lUuT5XJDj1p0bLO5rXKrfXwA&usqp=CAc" alt="">
@@ -156,8 +156,9 @@ $(document).ready(function () {
                                     var imagePath = imagen.url;
                                     imagePath = obtenerURLImagen(imagePath, SERVERURL);
                                     thumbnailsHtml += `
-                                <a class="list-group-item list-group-item-action" style="max-width: 100px !important; max-height: 100px !important; padding:0;" id="list-image-extra${index + 1}-list" data-bs-toggle="list" href="#list-image-extra${index + 1}" role="tab" aria-controls="image-extra${index + 1}">
-                                    <img src="${imagePath}" class="img-thumbnail">
+                                    
+                                <a id="list-image-extra${index + 1}-list" data-bs-toggle="list" href="#list-image-extra${index + 1}" role="tab" aria-controls="image-extra${index + 1}">
+                                    <img class="rounded border " width="60" height="60" src="${imagePath}" alt="">
                                 </a>
                             `;
                                 });
